e323f87e20a787b7cbedfe367e30d049f7776781
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2016-12-05 17:56:48 -0800

    Adding core eventing types

---
+++ src/WebJobs.Script/Eventing/ScriptEventManager.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Microsoft.Azure.WebJobs.Script.Eventing
+{
+    public class ScriptEventManager
+    {
+        private static readonly Subject<IScriptEvent> _subject = new Subject<IScriptEvent>();
+
+        public IObservable<IScriptEvent> Events => _subject.AsObservable();
+
+        public void Publish(IScriptEvent scriptEvent)
+        {
+            _subject.OnNext(scriptEvent);
+        }
+    }
+}
