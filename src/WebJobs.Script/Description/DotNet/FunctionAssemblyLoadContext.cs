f80886a181a0a90b2c160fc516666c7a49e49867
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2018-04-15 21:46:11 -0700

    Tmp: native resolution

--- src/WebJobs.Script/Description/DotNet/FunctionAssemblyLoadContext.cs
+++ src/WebJobs.Script/Description/DotNet/FunctionAssemblyLoadContext.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
+using System.Text.RegularExpressions;
 using System.Threading;
 using Microsoft.Azure.WebJobs.Script.Config;
 using Microsoft.Extensions.DependencyModel;
@@ -25,6 +26,7 @@ namespace Microsoft.Azure.WebJobs.Script.Description
     {
         private readonly string _baseProbingPath;
         private static readonly Lazy<string[]> _runtimeAssemblies = new Lazy<string[]>(GetRuntimeAssemblies);
+        private static readonly Lazy<Regex> _unmanagedFileNameRegex = new Lazy<Regex>(CreateUnmanagedFileNameRegex);
 
         private static Lazy<FunctionAssemblyLoadContext> _defaultContext = new Lazy<FunctionAssemblyLoadContext>(() => new FunctionAssemblyLoadContext(ResolveFunctionBaseProbingPath()), true);
 
@@ -56,6 +58,25 @@ namespace Microsoft.Azure.WebJobs.Script.Description
             }
 
             return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            // For now, we'll attempt to resolve unmanaged DLLs from the base probing path
+
+            // Directory resolution is be simple for now, we'll assume the base probing
+            // path (usually, the bin folder in the function app root), but we need to properly resolve
+            // the native module in different platforms:
+            // - Windows will append the '.DLL' extension to the name
+            // - Linux uses the 'lib' prefix and '.so' suffix. The version may also be appended to the suffix
+            // - macOS uses the 'lib' prefix '.dylib'
+            // To handle the different scenarios described above, we'll just have a pattern that gives us the ability
+            // to match the variations across different platforms. If needed, we can expand this in the future to have
+            // logic specific to the platform we're running under.
+            string pattern = "";
+            Directory.EnumerateFiles(_baseProbingPath, )
+
+            return base.LoadUnmanagedDll(unmanagedDllName);
         }
 
         protected static string ResolveFunctionBaseProbingPath()
@@ -91,6 +112,12 @@ namespace Microsoft.Azure.WebJobs.Script.Description
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        private static Regex CreateUnmanagedFileNameRegex()
+        {
+            string pattern = @"(^(?i)testname\.dll$|(?-i)^libtestname\.(so(\.\d+\.\d+\.\d+)?$|dylib$))";
+            var regex = new Regex(pattern);
         }
     }
 }
